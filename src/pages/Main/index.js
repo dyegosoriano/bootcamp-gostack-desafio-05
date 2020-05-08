e3314434ae809c01c19a2e5713c5f1ab9baa903b
Dyego Soriano <dyegodf2@gmail.com>
2020-05-08 13:59:58 -0300

    add estilos globais

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-// import { Container } from './styles'
+import { Tittle } from './styles';
 
 export default function Main() {
-  return <h1>Main</h1>;
+  return <Tittle>Main</Tittle>;
 }
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-// import { Container } from './styles'
+import { Container } from './styles';
 
 export default function Repository() {
-  return <h1>Repository</h1>;
+  return (
+    <Container>
+      <h1>Repository</h1>
+    </Container>
+  );
 }
